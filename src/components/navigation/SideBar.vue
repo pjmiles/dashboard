27e0fc771404e6fa828b716bcef082edd4529c6b
Ponmile Adebisi <ponmile@gridirontest.com>
2024-11-12 20:20:53 +0100

    adjusted styling

--- src/components/navigation/SideBar.vue
+++ src/components/navigation/SideBar.vue
@@ -6,7 +6,7 @@
 //   },
 // })
 import { ref } from 'vue'
-import avatar from '@/assets/avatarimg.jpg'
+import avatar from '@/assets/avatar.jpg'
 import navigationItems from './navigationItems'
 import logo from '@/assets/logo.jpg'
 
@@ -19,20 +19,22 @@ const setActiveNav = (index) => {
 </script>
 
 <template>
-  <div class="bg-purple-200 w-[250px] h-screen rounded-l-lg flex flex-col">
+  <div class="bg-[#f2e9e0] w-[200px] h-screen rounded-l-lg flex flex-col">
     <!-- logo -->
     <div class="flex space-x-1 justify-center p-8">
-      <img alt="logo" :src="logo" class="w-8 h-8"/>
+      <img alt="logo" :src="logo" class="w-8 h-8" />
       <h1 class="font-medium text-lg">Crowz</h1>
     </div>
     <!-- user profile -->
-    <div class="flex flex-col justify-center items-center p-4">
-      <img alt="avatar" :src="avatar" class="rounded-full w-[60px] h-[60px]" />
-      <p class="mt-2 text-2xl">Robert Grant</p>
-      <p class="text-sm text-gray-500">Managing Director</p>
+    <div class="flex flex-col justify-center items-center">
+      <div class="bg-orange-200 rounded-full">
+        <img alt="avatar" :src="avatar" class="rounded-full w-[50px] h-[50px]" />
+      </div>
+      <p class="mt-2 font-bold">Robert Grant</p>
+      <p class="text-[8px] text-gray-500">Managing Director</p>
     </div>
     <!-- navigation -->
-    <ul class="flex flex-col flex-grow justify-center items-center gap-3 mt-[-80px]">
+    <ul class="flex flex-col flex-grow justify-center items-center gap-[20px]">
       <li
         v-for="(item, index) in navigationItems"
         :key="index"
@@ -42,22 +44,24 @@ const setActiveNav = (index) => {
         <router-link
           v-if="item.path"
           :to="item.path"
-          class="flex items-center gap-2 text-sm"
-          :class="{ 'font-medium': activeNav === index, 'hover:font-medium': true }"
+          class="flex items-center gap-2 text-[12px]"
+          :class="{ 'font-bold': activeNav === index, 'hover:font-bold': true }"
         >
           <p @mouseover="isHovered = index" @mouseleave="isHovered = null">{{ item.label }}</p>
           <span v-if="item.icon" v-show="isHovered === index">
-            <p class="w-5 bg-red-400 text-white rounded-full text-center text-sm">{{ item.icon }}</p>
+            <p class="w-5 bg-red-400 text-white rounded-full text-center text-sm">
+              {{ item.icon }}
+            </p>
           </span>
         </router-link>
       </li>
     </ul>
     <!-- log out -->
-    <div class="flex justify-center space-x-1 mb-[50px]">
-      <p class="bg-black text-white rounded-full w-6 text-center cursor-pointer">
-        <i class="pi pi-arrow-circle-right"></i>
+    <div class="flex justify-center space-x-1 mb-2">
+      <p class="bg-black text-white rounded-full w-5 h-5 text-center cursor-pointer">
+        <i class="pi pi-sign-in text-sm"></i>
       </p>
-      <p>Log out</p>
+      <p class="text-[12px]">Log out</p>
     </div>
   </div>
 </template>
--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,48 +1,58 @@
 <template>
   <div class="w-full p-[60px]">
     <div class="flex gap-9">
-      <div class="w-full flex justify-between">
-        <p class="text-3xl">DASHBOARD</p>
-        <span class="space-x-5 mt-1">
-          <i class="pi pi-info-circle bg-red-400 text-white text-lg rounded-lg cursor-pointer"></i>
-          <i class="pi pi-cog text-lg cursor-pointer"></i>
-        </span>
+      <div>
+        <div class="w-full flex justify-between">
+          <p class="text-3xl font-semibold">Dashboard</p>
+          <span class="space-x-5 mt-1 mr-[112px]">
+            <i
+              class="pi pi-info-circle bg-red-400 text-white text-lg rounded-lg cursor-pointer"
+            ></i>
+            <i class="pi pi-cog text-lg cursor-pointer"></i>
+          </span>
+        </div>
+        <div class="grid grid-cols-3 w-[600px] mt-[50px]">
+          <div>
+            <span class="flex items-center space-x-5">
+              <div class="border-b border-gray-300 pb-1 flex items-center space-x-5">
+                <p class="text-xs">Views</p>
+                <i
+                  class="pi pi-info-circle bg-green-400 text-white text-lg rounded-full cursor-pointer"
+                ></i>
+              </div>
+            </span>
+            <p class="text-2xl">27,6m</p>
+          </div>
+          <div>
+            <span class="flex items-center space-x-5">
+              <div class="border-b border-gray-300 pb-1 flex items-center space-x-5">
+                <p class="text-xs">Followers</p>
+                <i
+                  class="pi pi-info-circle bg-red-400 text-white text-lg rounded-full cursor-pointer"
+                ></i>
+              </div>
+            </span>
+            <p class="text-2xl">219,3k</p>
+          </div>
+          <div>
+            <span class="flex items-center space-x-5">
+              <div class="border-b border-gray-300 pb-1 flex items-center space-x-5">
+                <p class="text-xs">Reports</p>
+                <i
+                  class="pi pi-info-circle bg-green-400 text-white text-lg rounded-full cursor-pointer"
+                ></i>
+              </div>
+            </span>
+            <p class="text-2xl">1,5k</p>
+          </div>
+        </div>
       </div>
-      <div class="rounded-lg border border-blue-600 w-3/5">
-        <!-- <img :src="upgradeImg" class=""/> -->
+      <div class="mt-[-30px]">
+        <img :src="upgradeImg" class="w-[400px]" />
       </div>
     </div>
-    <div class="grid grid-cols-3 mt-8 w-[800px]">
-      <div>
-        <span class="flex space-x-5">
-          <p>Views</p>
-          <i
-            class="pi pi-info-circle bg-green-400 text-white text-lg rounded-full mt-2 cursor-pointer"
-          ></i>
-        </span>
-        <hr />
-        <p>27,6m</p>
-      </div>
-      <div>
-        <span class="flex space-x-5">
-          <p>Followers</p>
-          <i
-            class="pi pi-info-circle bg-red-400 text-white text-lg rounded-full mt-2 cursor-pointer"
-          ></i>
-        </span>
-        <hr />
-        <p>219,3k</p>
-      </div>
-      <div>
-        <span class="flex space-x-5">
-          <p>Reports</p>
-          <i
-            class="pi pi-info-circle bg-green-400 text-white text-lg rounded-full mt-2 cursor-pointer"
-          ></i>
-        </span>
-        <hr />
-        <p>1,5k</p>
-      </div>
+    <div class="bg-[#e9f5f3] w-[1000px] h-[200px] rounded-lg mt-5">
+
     </div>
   </div>
 </template>
@@ -51,9 +61,4 @@
 import upgradeImg from '@/assets/upgradeImg.png'
 </script>
 
-<style scoped>
-/* img{
-  width: 100%;
-  height: 100%;
-} */
-</style>
+<style scoped></style>
